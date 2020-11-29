1b742b0246ddde0f8a4373ea8012251d2d9d1112
afar0308 <afdhalarrazy111@gmail.com>
2020-11-29 13:10:43 +0700

    Update Allowed Host Setting and Debug

--- asgard/settings.py
+++ asgard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')dmqriscby0=nq+qt6&xrf#pqachok!^+w972!5aqsd^n+-3l4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['asgard-ar-card.herokuapp.com', '127.0.0.1']
 
