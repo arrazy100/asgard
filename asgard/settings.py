0fc818756a117a67be49e5b37e030ab071bf6a0a
afar0308 <afdhalarrazy111@gmail.com>
2020-11-29 13:23:46 +0700

    Change Debug Value

--- asgard/settings.py
+++ asgard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')dmqriscby0=nq+qt6&xrf#pqachok!^+w972!5aqsd^n+-3l4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['asgard-ar-card.herokuapp.com', '127.0.0.1']
 
